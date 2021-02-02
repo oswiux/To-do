dfdee5990cbcba36258ef33b4e19a63fabaa6df4
oswiux <35800600+oswiux@users.noreply.github.com>
2021-02-02 12:52:02 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 import { Todo } from './components/Todo.js';
 
-const randomUzduotys = new Todo('Random uzduotys');
-const namuDarbai = new Todo('Namu darbai');
+const blynai = new Todo('Blynu kepimas');
 
+blynai.add('Uzmaisyti tesla');
+blynai.add('Ijungti kaitlente');
+blynai.add('Istraukti keptuve');
+blynai.add('Ipilti tesla i keptuve');
+blynai.add('Apversti blynus');
+blynai.add('Isimti blynus');
+blynai.add('Isjungti kaitlente');
 
-randomUzduotys.add('Sukurti pirma uzduoti');
-namuDarbai.add('Susitvarky Serloko projekta');
-namuDarbai.add('Susitvarky Serloko projekta');
-namuDarbai.add('Susitvarky Serloko projekta');
-randomUzduotys.add('Uzmaisyti tesla');
-namuDarbai.add('Susitvarky Serloko projekta');
-randomUzduotys.add('Nusiskusti');
-namuDarbai.add('Susitvarky Serloko projekta');
-namuDarbai.add('Susitvarky Serloko projekta');
+blynai.edit(0, 'Isplauti dubeni ir uzmaisyti tesla')
 
-console.log(randomUzduotys);
-console.log(namuDarbai);
+blynai.remove(4);
+blynai.taskCompleted(0);
+blynai.taskCompleted(1);
+blynai.taskCompleted(2);
 
-randomUzduotys.summary();
-namuDarbai.summary();
+blynai.summary();
 
-
-
-
-
-
-
+blynai.print();
+blynai.printCompletedOnly();
+blynai.printNotCompletedOnly();
